dc176213ee4f6b4a29439457dcdb8ad634f6fc4d
anderson.mo.carvalho <anderson.mo.carvalho@gmail.com>
2017-04-30 23:16:03 -0300

    BirthdayCakeCandle challenge included

---
+++ src/hacking/BirthdayCakeCandles.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hacking;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author anderson
+ */
+public class BirthdayCakeCandles {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int candles = in.nextInt();
+        int height[] = new int[candles];
+        int maxHeight = Integer.MIN_VALUE;
+        int maxCandlesBlowsCount = 0;
+        for (int i = 0; i < candles; i++) {
+            height[i] = in.nextInt();
+            if (height[i] > maxHeight){
+                maxCandlesBlowsCount = 1;
+                maxHeight = height[i];
+            } else if (height[i] == maxHeight){
+                maxCandlesBlowsCount++;
+            }
+        }
+        System.out.println(maxCandlesBlowsCount);
+    }
+}
